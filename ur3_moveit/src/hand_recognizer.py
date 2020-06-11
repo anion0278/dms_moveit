Fixed calculation of bound. radius with downsampling

ur3_moveit/src/hand_recognizer.py
@@ -13,7 +13,7 @@ import numpy as np
 from scipy.spatial import distance
 import custom_commander as com
 
-debug = True
+debug = False
 
 def get_color_range(color_base):
     sensitivity = 15
@@ -22,10 +22,13 @@ def get_color_range(color_base):
     return (lower, upper)
 
 # hue is in range 0..179
-right_color_range = get_color_range(60) #green
-left_color_range = get_color_range(15) #red
+right_color_range = get_color_range(60)  #green
+left_color_range = get_color_range(15)  #red
 
 blob_min_size = 50
+
+left_hmi = "hmi_left"
+right_hmi = "hmi_right"
 
 dwn_smpl = 8
 if debug:
@@ -48,7 +51,6 @@ def overlay_circle_on_img(image, pos, color):
                lineType=8,
                shift=0)
 
-
 def on_data(depth_msg, img_msg):
     img = bridge.imgmsg_to_cv2(img_msg, "bgr8")
     final_size = (img.shape[1] / dwn_smpl, img.shape[0] / dwn_smpl)
@@ -59,36 +61,45 @@ def on_data(depth_msg, img_msg):
     left_hand = find_hand(hsv, depth_msg, left_color_range)
 
     if right_hand is not None:
-        display_hand_on_img(img,right_hand, (255,0,0))
+        display_hand_on_img(img, right_hand, (255, 0, 0))
     if left_hand is not None:
-        display_hand_on_img(img,left_hand, (0,0,255))
+        display_hand_on_img(img, left_hand, (0, 0, 255))
 
     if left_hand is not None or right_hand is not None:
         depth_img = ros_numpy.numpify(depth_msg)
 
     if (right_hand is not None):
-        publish_hand(depth_img, depth_msg.header, right_hand, "hmi_right", right_pc_pub, debug)
+        publish_hand(depth_img, depth_msg.header, right_hand, right_hmi,
+                     right_pc_pub, debug)
     else:
-        driver.remove_hmi_obj("hmi_right")
+        driver.remove_hmi_obj(right_hmi)
         publish_emtpy_pc(right_pc_pub, depth_msg.header)
     if (left_hand is not None):
-        publish_hand(depth_img, depth_msg.header, left_hand, "hmi_left", left_pc_pub, debug)
+        publish_hand(depth_img, depth_msg.header, left_hand, left_hmi,
+                     left_pc_pub, debug)
     else:
-        driver.remove_hmi_obj("hmi_left")
+        driver.remove_hmi_obj(left_hmi)
         publish_emtpy_pc(left_pc_pub, depth_msg.header)
 
     cv2.imshow('image', img)
     cv2.waitKey(1)
 
-def publish_hand(depth_img, header, hand_tuple, obj_name, pc_pub, publish_pointcloud = False):
+
+def publish_hand(depth_img,
+                 header,
+                 hand_tuple,
+                 obj_name,
+                 pc_pub,
+                 publish_pointcloud=False):
     cloud_center = get_img_coords(depth_img, hand_tuple[0])
-    most_distant_point = max(hand_tuple[2], key=lambda p: distance.euclidean(p, hand_tuple[0]))
-    radius = distance.euclidean(most_distant_point, hand_tuple[0]) / 1450 # magic const
-    #radius = get_bounding_radius(depth_img, hand_tuple[0], most_distant_point[0])
+    radius = get_bounding_radius(hand_tuple[0], hand_tuple[2])
     driver.update_hmi_obj(cloud_center, header.frame_id, obj_name, radius)
 
     if publish_pointcloud:
-        cv2.drawContours(hand_tuple[3], [hand_tuple[2]], 0, color=255, thickness=-1)
+        cv2.drawContours(hand_tuple[3], [hand_tuple[2]],
+                         0,
+                         color=255,
+                         thickness=-1)
         blob_pts = np.where(hand_tuple[3] > 0)
         pc_hand = []
         for x, y in zip(blob_pts[0], blob_pts[1]):
@@ -99,6 +110,7 @@ def publish_hand(depth_img, header, hand_tuple, obj_name, pc_pub, publish_pointc
                 pass
         cloud_modified = pc2.create_cloud(header, pc_fields, pc_hand)
         pc_pub.publish(cloud_modified)
+
 
 def find_hand(hsv_img, depth_msg, color_range):
     result = None
@@ -113,21 +125,24 @@ def find_hand(hsv_img, depth_msg, color_range):
             result = (center, rect, blob, mask)
     return result
 
+
 def publish_emtpy_pc(pub, header):
     cloud_empty = pc2.create_cloud(header, pc_fields, [])
     pub.publish(cloud_empty)
 
+
 def display_hand_on_img(img, bound_box, color):
-    overlay_circle_on_img(img, bound_box[0],color)
+    overlay_circle_on_img(img, bound_box[0], color)
     box = cv2.boxPoints(bound_box[1])
-    box = np.int0(box) # what it does?
+    box = np.int0(box)  # what it does?
     cv2.drawContours(img, [box], 0, color, 2)
 
-def get_bounding_radius(depth_img, box_center, most_dist_hand_point):
-    corner_1 = get_img_coords(depth_img, box_center)[:-1]
-    corner_3 = get_img_coords(depth_img, most_dist_hand_point)[:-1]
-    radius = distance.euclidean(corner_1, corner_3)
-    return radius
+
+def get_bounding_radius(center, blob):
+    most_distant_point = max(blob, key=lambda p: distance.euclidean(p, center))
+    scale2d = 1450  # magic const
+    return distance.euclidean(most_distant_point, center) / scale2d * dwn_smpl
+
 
 def get_img_coords(depth_img, point):
     x = depth_img[point[1] * dwn_smpl, point[0] * dwn_smpl]["x"]
@@ -139,8 +154,6 @@ def get_img_coords(depth_img, point):
 if __name__ == "__main__":
     driver = robot_driver.RobotDriver(total_speed=0.2)
 
-    # commander = com.Commander(driver, num_attempts=-1)
-
     bridge = CvBridge()
     # rospy.init_node('top_cam_handler')
     depth_sub = message_filters.Subscriber("/camera_top/depth/color/points",
@@ -149,10 +162,16 @@ if __name__ == "__main__":
     camera_sub = message_filters.Subscriber("/camera_top/color/image_raw",
                                             Image,
                                             queue_size=1)
-    sync = message_filters.ApproximateTimeSynchronizer([depth_sub, camera_sub], queue_size=1, slop=0.05)
+    sync = message_filters.ApproximateTimeSynchronizer([depth_sub, camera_sub],
+                                                       queue_size=1,
+                                                       slop=0.05)
     sync.registerCallback(on_data)
 
-    right_pc_pub = rospy.Publisher("right_hmi_points", PointCloud2, queue_size=1)
-    left_pc_pub = rospy.Publisher("left_hmi_points", PointCloud2, queue_size=1)
+    right_pc_pub = rospy.Publisher(right_hmi + "_points",
+                                   PointCloud2,
+                                   queue_size=1)
+    left_pc_pub = rospy.Publisher(left_hmi + "_points",
+                                  PointCloud2,
+                                  queue_size=1)
 
     rospy.spin()