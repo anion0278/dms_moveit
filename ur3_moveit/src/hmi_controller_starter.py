d9d65b79a7e9667cbf6721e3dfbd8860b8d8a9c1
anion0278@gmail.com <anion0278@gmail.com>
2020-12-30 16:34:21 +0100

    Renamed files

--- ur3_moveit/src/hmi_controller_starter.py
+++ ur3_moveit/src/hmi_controller_starter.py
@@ -11,7 +11,7 @@ import config
 
 # no other way to start it correctly 
 rospy.init_node('hmi_starter', anonymous=True)
-os.system("rosrun ur3_moveit hmi_disconnector.py")
+os.system("rosrun ur3_moveit hmi_controller_disconnector.py")
 
 # by starting via rosrun the amout of unrelated info is rediced
 rp.run_process_async("rosrun ur3_moveit hmi_controller.py hmi_left node")
