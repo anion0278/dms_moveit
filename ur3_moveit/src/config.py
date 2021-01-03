Fixed module names Tracker: Fixed coordinates calculation Controller: refactoring of notification managment Contorller: added notification modes and configurable notificaitons

ur3_moveit/src/config.py
@@ -6,11 +6,21 @@ class Side(Enum):
     LEFT = 1
     RIGHT = 2
 
+class TaskStatus(Enum):
+    OK = 0
+    REPLANNING = 1
+    INVALID_GOAL = 2
+
 hmi_right = "hmi_right"
 hmi_left = "hmi_left"
 
 status_replan = "replan"
 status_invalid = "invalid"
+
+task_status_param_name = "task_status"
+clearance_param_name = "/move_group/collision/min_clearance"
+
+collision_vec_topic = "/move_group/collision_vectors"
 
 invalid_goal_intensity = 220 # replan stronger than invalid goal, because is more frequents
 replan_intensity = 250
@@ -24,8 +34,6 @@ color_right = np.array([1, 0, 0])
 calibr_service = "_frame_calibration"
 offsets_file = "{0}_imu_offsets.p"
 calibr_file = "{0}_frame_calibration.p"
-
-collision_vec_topic = "/move_group/collision_vectors"
 
 current_script_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -49,8 +57,7 @@ def get_visual_color(device_name):
 
 def get_rviz_color(device_name):
     c = get_visual_color(device_name)
-    c.append(0.7) # alpha
-    return c
+    return np.append(c, 0.7) # alpha
 
 def get_second_hmi_name(first_hmi_name):
     left = "left"
@@ -63,3 +70,4 @@ def get_second_hmi_name(first_hmi_name):
 
 def get_side(hmi_name):
     return hmi_name.replace("hmi_", "")
+    

ur3_moveit/src/hmi_controller_processor.py
@@ -28,7 +28,7 @@ class DataProcessor():
         self.current_orientation = None
         self.current_imu_status = None
 
-    def get_speed_vector(self, speed):
+    def get_speed_vector(self):
         vs = Vector3Stamped(vector = Vector3(*self.currect_collision_vec))
         qa = quaternion_inverse(ros_numpy.numpify(self.current_orientation))
         trs = TransformStamped(transform = Transform(rotation = Quaternion(*qa)))
@@ -53,8 +53,10 @@ class DataProcessor():
         if any(data_namespace == m.ns for m in marker_array_msg.markers):
             points = (m.points for m in marker_array_msg.markers if data_namespace == m.ns).next()
             v = ros_numpy.numpify(points[0]) - ros_numpy.numpify(points[1])
-            self.currect_collision_vec = v / np.linalg.norm(v) # vector should be normalized !
+            vec_len = np.linalg.norm(v)
+            self.currect_collision_vec = v / vec_len # vector should be normalized !
             #print("New vector: %s" % self.currect_collision_vec)
+            print("New vector length [m]: %s" % vec_len)
 
     def __get_compressed_quarternion(self, data): 
         floats = np.zeros(4)