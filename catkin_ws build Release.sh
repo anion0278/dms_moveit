e5266e8403ff0348aa8d5add158ba36db4a3df4d
anion0278@gmail.com <anion0278@gmail.com>
2021-01-11 18:25:48 +0100

    Updated robot xacro definition to use UR Driver

--- catkin_ws build Release.sh
+++ catkin_ws build Release.sh
@@ -3,5 +3,5 @@
 source /opt/ros/melodic/setup.bash
 source ~/catkin_ws/devel/setup.bash
 cd ~/catking_ws
-catkin config --cmake-args -DCMAKE_BUILD_TYPE=Release
-catkin build --jobs 3 -DCATKIN_ENABLE_TESTING=0
+catkin config  --extend /opt/ros/${ROS_DISTRO} --cmake-args -DCMAKE_BUILD_TYPE=Release
+catkin build --jobs 5
