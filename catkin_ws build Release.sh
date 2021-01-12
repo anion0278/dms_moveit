7711ff0266be5deabb5857a0f90dd15b5c594beb
anion0278@gmail.com <anion0278@gmail.com>
2021-01-12 18:28:16 +0100

    Changed definition of build for main catkin ws Added catkin installation of packages

--- catkin_ws build Release.sh
+++ catkin_ws build Release.sh
@@ -2,6 +2,6 @@
 
 source /opt/ros/melodic/setup.bash
 source ~/catkin_ws/devel/setup.bash
-cd ~/catking_ws
-catkin config  --extend /opt/ros/${ROS_DISTRO} --cmake-args -DCMAKE_BUILD_TYPE=Release
-catkin build --jobs 5
+cd ~/catkin_ws
+catkin_make  --jobs 5 -DCATKIN_ENABLE_TESTING=False -DCMAKE_BUILD_TYPE=Release
+catkin_make install
