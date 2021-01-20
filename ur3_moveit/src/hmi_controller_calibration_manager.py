Fixed "Ignoring transform" TF error All quaternions should be normalized !

ur3_moveit/src/hmi_controller_calibration_manager.py
@@ -5,7 +5,6 @@ from std_srvs.srv import TriggerRequest, Trigger, TriggerResponse
 import pickle as serializer # replace by json if more readable data format is needed
 from tf.transformations import *
 from geometry_msgs.msg import Quaternion
-import ros_numpy
 import tf2_ros
 
 import config
@@ -36,10 +35,10 @@ class CalibrationManager():
 
     def publish_tf(self, quaternion_hmi):
         if self.debug: 
-            tfs_north_direction = util_ros.get_stamped_transform(self.__tracked_frame_id, self.zero_frame_id, Quaternion(*self.frame_calibration))
+            tfs_north_direction = util_ros.get_stamped_transform(self.__tracked_frame_id, self.zero_frame_id, util_ros.get_norm_quaternion_msg(self.frame_calibration))
             self.tf_pub.sendTransform(tfs_north_direction) # correct only when calibrated
         
-        tfs_calibrated = util_ros.get_stamped_transform(self.__tracked_frame_id, self.calibr_frame_id, Quaternion(*quaternion_hmi))
+        tfs_calibrated = util_ros.get_stamped_transform(self.__tracked_frame_id, self.calibr_frame_id, util_ros.get_norm_quaternion_msg(quaternion_hmi))
         self.tf_pub.sendTransform(tfs_calibrated)
 
     def start_calibration_service(self):

ur3_moveit/src/hmi_tracker_image_processor.py
@@ -51,8 +51,8 @@ class HmiTrackerImageProcessor:
         return ColorRange(lower, upper)
 
     def get_hsv_red_range(self):
-        lower = (-30, 80, 90)
-        upper = (10, 255, 255) # 150 - 179
+        lower = (-30, 80, 90) # 150 - 179
+        upper = (10, 255, 255) 
         return ColorRange(lower, upper)
 
     def get_hsv_color_range(self, color_base):
@@ -151,7 +151,8 @@ class HmiTrackerImageProcessor:
 
                 if not self.is_center_within_contour(center, contour_pts): # ZERO meaning the point is on the contour!!!
                     # the closest point INSIDE contour, otherwise the center's height can be incorreclty calculated
-                    center = np.squeeze(min(contour_pts, key=lambda p: distance.euclidean(p, center))) # TODO takes point from contour, which sometimes out of shape (especially when on the edge of image)
+                    # TODO takes point from contour, which sometimes out of shape (especially when on the edge of image)
+                    center = np.squeeze(min(contour_pts, key=lambda p: distance.euclidean(p, center))) 
 
                 result = HmiImageData(center, rect, contour_pts, color_mask, single_hand_mask)
         return result

ur3_moveit/src/util_ros_msgs.py
@@ -1,5 +1,6 @@
 from geometry_msgs.msg import *
 import rospy
+import numpy as np
 
 def get_stamped_transform(frame_id, trf_frame_id, rotation):
     tfs = TransformStamped(transform = Transform(rotation = rotation))
@@ -11,5 +12,9 @@ def get_stamped_transform(frame_id, trf_frame_id, rotation):
 def get_identity_quat_arr():
     return [0,0,0,1]
 
-def get_identity_quat():
+def get_identity_quat_msg():
     return Quaternion(*get_identity_quat_arr())
+
+def get_norm_quaternion_msg(quaternion_data): # quaternions must be normalized, otherwise TF will show errors
+    quat_norm = quaternion_data / np.linalg.norm(quaternion_data)
+    return Quaternion(*quat_norm)