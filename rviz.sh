ba41f86c0ecd95f0bc4734091b1ea2e781808659
anion0278@gmail.com <anion0278@gmail.com>
2021-01-26 11:33:12 +0100

    Added separate Rviz configuration for the project

--- rviz.sh
+++ rviz.sh
@@ -4,4 +4,5 @@ source /opt/ros/melodic/setup.bash
 source ~/catkin_ws/devel/setup.bash
 
 #roslaunch ur3_moveit start_rviz.launch
-rviz
+#rviz
+rosrun rviz rviz -d `rospack find ur3_moveit`/launch/dms_rviz.rviz
